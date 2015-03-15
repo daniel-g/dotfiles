d11ee01f79027d919827bb70f38fc1255be8949b
Daniel Gaytan <dgaytan6@gmail.com>
2015-03-15 10:47:44 -0600

    Create link sh file to update links in local

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -f -s $PWD/.gitconfig   ~/.gitconfig
+ln -f -s $PWD/.gitignore   ~/.gitignore
+ln -f -s $PWD/.gitk        ~/.gitk
+ln -f -s $PWD/.rspec       ~/.rspec
+ln -f -s $PWD/.tmux.conf   ~/.tmux.conf
+ln -f -s $PWD/.vimrc.local ~/.vimrc.local
