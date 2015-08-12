e4f129785c8b8ebf1210982d487cbb2310d9034e
Daniel Gaytan <dgaytan6@gmail.com>
2015-08-12 11:54:22 -0500

    Remove gitk, tmux, and vimrc from my dotfiles link.sh runner

--- link.sh
+++ link.sh
@@ -2,7 +2,4 @@
 
 ln -f -s $PWD/.gitconfig   ~/.gitconfig
 ln -f -s $PWD/.gitignore   ~/.gitignore
-ln -f -s $PWD/.gitk        ~/.gitk
 ln -f -s $PWD/.rspec       ~/.rspec
-ln -f -s $PWD/.tmux.conf   ~/.tmux.conf
-ln -f -s $PWD/.vimrc.local ~/.vimrc.local
